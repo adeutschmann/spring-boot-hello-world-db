3d608336fa29dc3a05bd97c0dc82827419a705e3
Alexander Deutschmann <a.deutschmann@outlook.com>
2025-10-07 23:52:05 +0200

    feat: remove unused server configurations from OpenApiConfig

--- src/main/java/src/main/java/ch/adeutschmanndev/helloworlddb/config/OpenApiConfig.java
+++ src/main/java/src/main/java/ch/adeutschmanndev/helloworlddb/config/OpenApiConfig.java
@@ -3,12 +3,9 @@ package ch.adeutschmanndev.helloworlddb.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -23,15 +20,7 @@ public class OpenApiConfig {
                         .contact(new Contact()
                                 .name("API Support")
                                 .email("support@example.com")
-                                .url("https://example.com")))
-                .servers(List.of(
-                        new Server()
-                                .url("http://localhost:8080")
-                                .description("Development server"),
-                        new Server()
-                                .url("https://api.example.com")
-                                .description("Production server")
-                ));
+                                .url("https://example.com")));
     }
 
     @Bean
